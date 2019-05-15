a579f2ab4759fe89d69ca4bfc39146d1dbe11dfe
Harkur <kwek.kurniawan@gmail.com>
2019-05-15 08:22:33 +0700

    Initial Commit

---
+++ FirstGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstGitProject
+//
+//  Created by Hartono Kurniawan on 15/05/19.
+//  Copyright © 2019 Hartono Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
